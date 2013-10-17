ca12a18ef5268ecc158a6b99ccf6f68665c53106
lazy <lr_5146@163.com>
2013-10-17 19:55:13 +0800

    Initial Commit

---
+++ WordRichText/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WordRichText
+//
+//  Created by lazy on 13-10-17.
+//  Copyright (c) 2013年 lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WordRichText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WordRichText
+//
+//  Created by lazy on 13-10-17.
+//  Copyright (c) 2013年 lazy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
